8c09d63eb6e931d7d974df8328cca0ba6139e65f
Zneeky <arkanahmedov02@gmail.com>
2023-09-14 10:29:34 +0300

    Fixes

--- Client/BBESConsultReactJS/src/pages/home.tsx
+++ Client/BBESConsultReactJS/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import TopNavBar from "../components/top-navbar";
+//import TopNavBar from "../components/top-navbar";
 import HeroSection from "../sections/hero-section";
 
 const HomePage = () => {
